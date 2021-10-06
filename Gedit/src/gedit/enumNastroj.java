b2f3cedb2212ffb64a5a78ec0931f8ae4ad88603
Jirka <pisejiricz@gmail.com>
2021-10-06 11:27:22 +0200

    malování

---
+++ Gedit/src/gedit/enumNastroj.java
@@ -0,0 +1,17 @@
+package gedit;
+
+public enum enumNastroj {
+    TUZKA("Tužka"),
+    OBDELNIK("Obdelník"),
+    KRUH("Kruh"),
+    GUMA("Guma");
+    
+    
+    private String jmeno;
+
+    private enumNastroj(String jmeno) {
+        this.jmeno = jmeno;
+    }
+    
+    
+}
