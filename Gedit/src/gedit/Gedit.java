aa32ad9f03301b8119cfed64c6e0c689c2e32e87
Jirka <pisejiricz@gmail.com>
2021-10-12 10:29:51 +0200

    obdelnik, guma, rozmer

--- Gedit/src/gedit/Gedit.java
+++ Gedit/src/gedit/Gedit.java
@@ -17,7 +17,10 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
@@ -33,18 +36,27 @@ public class Gedit extends Application {
 
     Canvas canvas = null;
     Point2D lineStart = null;
+    Point2D rectStart = null;
     private Color currColor = Color.RED;
     BorderPane root = new BorderPane();
     HBox hbox = new HBox();
     ColorPicker colorPicker = new ColorPicker(currColor);
-    GraphicsContext gc ;
+    GraphicsContext gc;
+    ComboBox<enumNastroj> comboboxNastroje = new ComboBox<>();
+    Button buttonClear = new Button("Clear");
+    Spinner<Integer> spinnerSirkaCary = new Spinner<>();
 
     @Override
     public void start(Stage primaryStage) {
 
         canvas = new Canvas();
         gc = canvas.getGraphicsContext2D();
+        comboboxNastroje.getItems().addAll(enumNastroj.values());
+        comboboxNastroje.getSelectionModel().select(enumNastroj.TUZKA);
+        SpinnerValueFactory valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 20, 2);
+        spinnerSirkaCary.setValueFactory(valueFactory);
         
+
         colorPicker.setOnAction((ActionEvent event) -> {
             currColor = colorPicker.getValue();
         });
@@ -56,8 +68,12 @@ public class Gedit extends Application {
         canvas.heightProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
             draw();
         });
+        
+        buttonClear.setOnAction((event) -> {
+            clearAll();
+        });
 
-        hbox.getChildren().addAll(colorPicker);
+        hbox.getChildren().addAll(colorPicker, comboboxNastroje,buttonClear,spinnerSirkaCary);
         root.setCenter(canvas);
         root.setTop(hbox);
 
@@ -76,15 +92,55 @@ public class Gedit extends Application {
     }
 
     private void draw() {
-        if (canvas != null) {    
+        if (canvas != null) {
             canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, (MouseEvent event) -> {
-                gc.setStroke(currColor);
-                lineStart = new Point2D(event.getX(), event.getY());
+                if (null != comboboxNastroje.getValue()) {
+                    switch (comboboxNastroje.getValue()) {
+                        case TUZKA:
+                            gc.setStroke(currColor);
+                            gc.setLineWidth(spinnerSirkaCary.getValue());
+                            lineStart = new Point2D(event.getX(), event.getY());
+                            break;
+                        case GUMA:
+                            gc.setStroke(Color.WHITESMOKE);
+                            lineStart = new Point2D(event.getX(), event.getY());
+                            break;
+                        case OBDELNIK:
+                            rectStart = new Point2D(event.getX(), event.getY());
+                            break;
+                        case KRUH:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
             });
 
             canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, (MouseEvent event) -> {
-                gc.strokeLine(lineStart.getX(), lineStart.getY(), event.getX(), event.getY());
-                lineStart = new Point2D(event.getX(), event.getY());
+                if (null != comboboxNastroje.getValue()) {
+                    switch (comboboxNastroje.getValue()) {
+                        case TUZKA:
+                            gc.strokeLine(lineStart.getX(), lineStart.getY(), event.getX(), event.getY());
+                            gc.setLineWidth(spinnerSirkaCary.getValue());
+                            lineStart = new Point2D(event.getX(), event.getY());
+                            break;
+                        case GUMA:
+                            gc.strokeLine(lineStart.getX(), lineStart.getY(), event.getX(), event.getY());
+                            gc.setLineWidth(spinnerSirkaCary.getValue() * 3);
+                            lineStart = new Point2D(event.getX(), event.getY());
+                            break;
+                        case OBDELNIK:
+                            gc.setFill(currColor);
+                            gc.fillPolygon(new double[]{rectStart.getX(), rectStart.getX(), event.getX(), event.getX()}, new double[]{rectStart.getY(), event.getY(), event.getY(), rectStart.getY()}, 4);
+                            break;
+                        case KRUH:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
             });
 
             canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
